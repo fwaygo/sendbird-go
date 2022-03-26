1fa8fa052f5eb8e24f27245bf8f9e408f392d02f
epletcher72 <stevenjang75@yahoo.com>
2022-03-25 18:33:39 -0700

    go get

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.17
 
 require github.com/kelseyhightower/envconfig v1.4.0
 
-require github.com/fwaygo/fwaygo-kit v1.19.0
+require github.com/fwaygo/fwaygo-kit v1.27.0
--- sendbird_test.go
+++ sendbird_test.go
@@ -2,7 +2,6 @@ package sendbird
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"testing"
 
@@ -67,225 +66,225 @@ func getConfig(t *testing.T) *Config {
 // 	log.Printf("Created Channel %s", channel.ChannelUrl)
 // }
 
-func TestSendMessage(t *testing.T) {
-	config := getConfig(t)
-	client, err := NewClient(ClientConfig{
-		ApplicationID: config.SendBirdAppID,
-		APIToken:      config.SendBirdAPIToken,
-		Version:       config.SendBirdVersion,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+// func TestSendMessage(t *testing.T) {
+// 	config := getConfig(t)
+// 	client, err := NewClient(ClientConfig{
+// 		ApplicationID: config.SendBirdAppID,
+// 		APIToken:      config.SendBirdAPIToken,
+// 		Version:       config.SendBirdVersion,
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	ctx := context.Background()
-	user1 := format.NewUserID()
-	user2 := format.NewUserID()
-	users := []string{string(user1), string(user2)}
-	channelName := string(user1) + ":" + string(user2)
+// 	ctx := context.Background()
+// 	user1 := format.NewUserID()
+// 	user2 := format.NewUserID()
+// 	users := []string{string(user1), string(user2)}
+// 	channelName := string(user1) + ":" + string(user2)
 
-	for _, j := range users {
-		user, err := client.UsersCreate(ctx, api.UsersCreateRequest{
-			UserID:     j,
-			Nickname:   j,
-			ProfileURL: "",
-		})
-		if err != nil {
-			t.Fatal(err)
-		}
-		log.Printf("New User %s: %s", user.UserID, user.CreatedAt)
-	}
+// 	for _, j := range users {
+// 		user, err := client.UsersCreate(ctx, api.UsersCreateRequest{
+// 			UserID:     j,
+// 			Nickname:   j,
+// 			ProfileURL: "",
+// 		})
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+// 		log.Printf("New User %s: %s", user.UserID, user.CreatedAt)
+// 	}
 
-	channel, err := client.ChannelsCreate(ctx, api.ChannelCreateRequest{
-		UserIDs: users,
-		Name:    &channelName,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	channel, err := client.ChannelsCreate(ctx, api.ChannelCreateRequest{
+// 		UserIDs: users,
+// 		Name:    &channelName,
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	log.Printf("Created Channel %s", channel.ChannelUrl)
-	request := api.SendBaseMessageRequest{
-		MessageType: "MESG",
-	}
+// 	log.Printf("Created Channel %s", channel.ChannelUrl)
+// 	request := api.SendBaseMessageRequest{
+// 		MessageType: "MESG",
+// 	}
 
-	message, err := client.SendUserMessage(ctx, api.SendUserMessageRequest{
-		SendBaseMessageRequest: request,
-		UserID:                 string(user1),
-		Message:                "first test message",
-	}, api.ChannelParams{
-		ChannelType: "group_channels",
-		ChannelUrl:  channel.ChannelUrl,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	message, err := client.SendUserMessage(ctx, api.SendUserMessageRequest{
+// 		SendBaseMessageRequest: request,
+// 		UserID:                 string(user1),
+// 		Message:                "first test message",
+// 	}, api.ChannelParams{
+// 		ChannelType: "group_channels",
+// 		ChannelUrl:  channel.ChannelUrl,
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	log.Printf("Message ID: %d", message.MessageID)
-}
+// 	log.Printf("Message ID: %d", message.MessageID)
+// }
 
-func TestReactions(t *testing.T) {
-	config := getConfig(t)
-	client, err := NewClient(ClientConfig{
-		ApplicationID: config.SendBirdAppID,
-		APIToken:      config.SendBirdAPIToken,
-		Version:       config.SendBirdVersion,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+// func TestReactions(t *testing.T) {
+// 	config := getConfig(t)
+// 	client, err := NewClient(ClientConfig{
+// 		ApplicationID: config.SendBirdAppID,
+// 		APIToken:      config.SendBirdAPIToken,
+// 		Version:       config.SendBirdVersion,
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	ctx := context.Background()
-	user1 := format.NewUserID()
-	user2 := format.NewUserID()
-	users := []string{string(user1), string(user2)}
-	channelName := string(user1) + ":" + string(user2)
+// 	ctx := context.Background()
+// 	user1 := format.NewUserID()
+// 	user2 := format.NewUserID()
+// 	users := []string{string(user1), string(user2)}
+// 	channelName := string(user1) + ":" + string(user2)
 
-	for _, j := range users {
-		user, err := client.UsersCreate(ctx, api.UsersCreateRequest{
-			UserID:     j,
-			Nickname:   j,
-			ProfileURL: "",
-		})
-		if err != nil {
-			t.Fatal(err)
-		}
-		log.Printf("New User %s: %s", user.UserID, user.CreatedAt)
-	}
+// 	for _, j := range users {
+// 		user, err := client.UsersCreate(ctx, api.UsersCreateRequest{
+// 			UserID:     j,
+// 			Nickname:   j,
+// 			ProfileURL: "",
+// 		})
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+// 		log.Printf("New User %s: %s", user.UserID, user.CreatedAt)
+// 	}
 
-	channel, err := client.ChannelsCreate(ctx, api.ChannelCreateRequest{
-		UserIDs: users,
-		Name:    &channelName,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	channel, err := client.ChannelsCreate(ctx, api.ChannelCreateRequest{
+// 		UserIDs: users,
+// 		Name:    &channelName,
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	log.Printf("Created Channel %s", channel.ChannelUrl)
-	request := api.SendBaseMessageRequest{
-		MessageType: "MESG",
-	}
+// 	log.Printf("Created Channel %s", channel.ChannelUrl)
+// 	request := api.SendBaseMessageRequest{
+// 		MessageType: "MESG",
+// 	}
 
-	message, err := client.SendUserMessage(ctx, api.SendUserMessageRequest{
-		SendBaseMessageRequest: request,
-		UserID:                 string(user1),
-		Message:                "first test message",
-	}, api.ChannelParams{
-		ChannelType: "group_channels",
-		ChannelUrl:  channel.ChannelUrl,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	message, err := client.SendUserMessage(ctx, api.SendUserMessageRequest{
+// 		SendBaseMessageRequest: request,
+// 		UserID:                 string(user1),
+// 		Message:                "first test message",
+// 	}, api.ChannelParams{
+// 		ChannelType: "group_channels",
+// 		ChannelUrl:  channel.ChannelUrl,
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	log.Printf("Message ID: %d", message.MessageID)
+// 	log.Printf("Message ID: %d", message.MessageID)
 
-	reaction, err := client.AddReaction(ctx, api.AddReactionRequest{
-		UserID:   string(user1),
-		Reaction: "celebrate",
-	}, api.ChannelParams{
-		ChannelType: api.GROUP_CHANNEL,
-		ChannelUrl:  channel.ChannelUrl,
-		MessageID:   &message.MessageID,
-	})
-	if err != nil {
-		t.Fatalf("could not add reaction -- %s", err)
-	}
+// 	reaction, err := client.AddReaction(ctx, api.AddReactionRequest{
+// 		UserID:   string(user1),
+// 		Reaction: "celebrate",
+// 	}, api.ChannelParams{
+// 		ChannelType: api.GROUP_CHANNEL,
+// 		ChannelUrl:  channel.ChannelUrl,
+// 		MessageID:   &message.MessageID,
+// 	})
+// 	if err != nil {
+// 		t.Fatalf("could not add reaction -- %s", err)
+// 	}
 
-	log.Printf("Reaction: %v", reaction)
-}
+// 	log.Printf("Reaction: %v", reaction)
+// }
 
-func TestListChannels(t *testing.T) {
-	config := getConfig(t)
-	client, err := NewClient(ClientConfig{
-		ApplicationID: config.SendBirdAppID,
-		APIToken:      config.SendBirdAPIToken,
-		Version:       config.SendBirdVersion,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+// func TestListChannels(t *testing.T) {
+// 	config := getConfig(t)
+// 	client, err := NewClient(ClientConfig{
+// 		ApplicationID: config.SendBirdAppID,
+// 		APIToken:      config.SendBirdAPIToken,
+// 		Version:       config.SendBirdVersion,
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	ctx := context.Background()
-	user1 := format.NewUserID()
-	user2 := format.NewUserID()
-	user3 := format.NewUserID()
+// 	ctx := context.Background()
+// 	user1 := format.NewUserID()
+// 	user2 := format.NewUserID()
+// 	user3 := format.NewUserID()
 
-	users1 := []string{user1.String(), user2.String()}
-	users2 := []string{user1.String(), user3.String()}
-	users3 := []string{user1.String(), user2.String(), user3.String()}
+// 	users1 := []string{user1.String(), user2.String()}
+// 	users2 := []string{user1.String(), user3.String()}
+// 	users3 := []string{user1.String(), user2.String(), user3.String()}
 
-	usersArr := [][]string{
-		users1,
-		users2,
-		users3,
-	}
-	channelName := user1.String() + ":" + user2.String()
+// 	usersArr := [][]string{
+// 		users1,
+// 		users2,
+// 		users3,
+// 	}
+// 	channelName := user1.String() + ":" + user2.String()
 
-	for _, j := range users3 {
-		user, err := client.UsersCreate(ctx, api.UsersCreateRequest{
-			UserID:     j,
-			Nickname:   j,
-			ProfileURL: "",
-		})
-		if err != nil {
-			t.Fatal(err)
-		}
-		log.Printf("New User %s: %s", user.UserID, user.CreatedAt)
-	}
+// 	for _, j := range users3 {
+// 		user, err := client.UsersCreate(ctx, api.UsersCreateRequest{
+// 			UserID:     j,
+// 			Nickname:   j,
+// 			ProfileURL: "",
+// 		})
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+// 		log.Printf("New User %s: %s", user.UserID, user.CreatedAt)
+// 	}
 
-	for _, users := range usersArr {
-		_, err := client.ChannelsCreate(ctx, api.ChannelCreateRequest{
-			UserIDs: users,
-			Name:    &channelName,
-		})
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
+// 	for _, users := range usersArr {
+// 		_, err := client.ChannelsCreate(ctx, api.ChannelCreateRequest{
+// 			UserIDs: users,
+// 			Name:    &channelName,
+// 		})
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+// 	}
 
-	for _, users := range usersArr {
+// 	for _, users := range usersArr {
 
-		channelList, err := client.ChannelsList(ctx, api.ChannelListRequest{
-			ShowEmpty:           true,
-			ShowMember:          false,
-			ShowDeliveryReceipt: false,
-			ShowReadReceipt:     false,
-			ShowMetadata:        false,
-			ShowFrozen:          false,
-			MembersExactlyIn:    users,
-		})
+// 		channelList, err := client.ChannelsList(ctx, api.ChannelListRequest{
+// 			ShowEmpty:           true,
+// 			ShowMember:          false,
+// 			ShowDeliveryReceipt: false,
+// 			ShowReadReceipt:     false,
+// 			ShowMetadata:        false,
+// 			ShowFrozen:          false,
+// 			MembersExactlyIn:    users,
+// 		})
 
-		for _, channel := range channelList.Channels {
-			fmt.Printf("Channel id: %s\n", channel.ChannelUrl)
-		}
+// 		for _, channel := range channelList.Channels {
+// 			fmt.Printf("Channel id: %s\n", channel.ChannelUrl)
+// 		}
 
-		if err != nil {
-			t.Fatal(err)
-		}
-		if len(channelList.Channels) != 1 {
-			t.Fatal("Query incorrect: request responded with multiple channels")
-		}
-	}
-	log.Println("Query Pass")
-}
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+// 		if len(channelList.Channels) != 1 {
+// 			t.Fatal("Query incorrect: request responded with multiple channels")
+// 		}
+// 	}
+// 	log.Println("Query Pass")
+// }
 
-func TestEncoder(t *testing.T) {
-	user1 := format.NewUserID()
-	user2 := format.NewUserID()
-	user3 := format.NewUserID()
-	users := []string{user1.String(), user2.String(), user3.String()}
+// func TestEncoder(t *testing.T) {
+// 	user1 := format.NewUserID()
+// 	user2 := format.NewUserID()
+// 	user3 := format.NewUserID()
+// 	users := []string{user1.String(), user2.String(), user3.String()}
 
-	EncodeParameters(&api.ChannelListRequest{
-		ShowEmpty:           true,
-		ShowMember:          false,
-		ShowDeliveryReceipt: false,
-		ShowReadReceipt:     false,
-		ShowMetadata:        false,
-		ShowFrozen:          false,
-		MembersExactlyIn:    users,
-	})
-}
+// 	EncodeParameters(&api.ChannelListRequest{
+// 		ShowEmpty:           true,
+// 		ShowMember:          false,
+// 		ShowDeliveryReceipt: false,
+// 		ShowReadReceipt:     false,
+// 		ShowMetadata:        false,
+// 		ShowFrozen:          false,
+// 		MembersExactlyIn:    users,
+// 	})
+// }
 
 // func TestAddMember(t *testing.T) {
 // 	config := getConfig(t)
@@ -348,3 +347,60 @@ func TestEncoder(t *testing.T) {
 // 		t.Fatalf("failed to add member: %s", err)
 // 	}
 // }
+
+func TestFileMessage(t *testing.T) {
+	config := getConfig(t)
+	client, err := NewClient(ClientConfig{
+		ApplicationID: config.SendBirdAppID,
+		APIToken:      config.SendBirdAPIToken,
+		Version:       config.SendBirdVersion,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+	user1 := format.NewUserID()
+	user2 := format.NewUserID()
+	users := []string{string(user1), string(user2)}
+	channelName := string(user1) + ":" + string(user2)
+
+	for _, j := range users {
+		user, err := client.UsersCreate(ctx, api.UsersCreateRequest{
+			UserID:     j,
+			Nickname:   j,
+			ProfileURL: "",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		log.Printf("New User %s: %s", user.UserID, user.CreatedAt)
+	}
+
+	channel, err := client.ChannelsCreate(ctx, api.ChannelCreateRequest{
+		UserIDs: users,
+		Name:    &channelName,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("Created Channel %s", channel.ChannelUrl)
+	request := api.SendBaseMessageRequest{
+		MessageType: "MESG",
+	}
+
+	message, err := client.SendFileMessage(ctx, api.SendFileMessageRequest{
+		SendBaseMessageRequest: request,
+		UserID:                 string(user1),
+		URL:                    "dummy",
+	}, api.ChannelParams{
+		ChannelType: "group_channels",
+		ChannelUrl:  channel.ChannelUrl,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("Message ID: %d", message.MessageID)
+}
